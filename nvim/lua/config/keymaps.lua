1a1b19038298f5ee7dba151045454c94aeb20a8a
Satya Prakash <satya@Satyas-MacBook-Pro.local>
2025-05-12 10:01:02 +0530

    [update] local changes for mbp

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,11 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set('n', '<C-p>', Snacks.picker.files, { noremap = true, silent = true })
+vim.keymap.set('n', '<C-l>', Snacks.picker.recent, { noremap = true, silent = true })
+
+vim.opt.shell = "/bin/zsh"
+
+vim.keymap.set("n", "q", ":wq<CR>", { noremap = true, silent = true })
+
--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,3 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+vim.cmd("cd ~/Documents")
+
+vim.o.guifont = "Hack Nerd Font Mono:h18"
